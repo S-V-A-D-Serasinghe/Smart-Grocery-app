a09d4677fa8b07c44887be9b018f093eabcbd8c3
S-V-A-D-Serasinghe <svadithadinuja@outlook.com>
2022-08-13 15:37:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zijsy1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zijsy1)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import th from '@angular/common/locales/extra/th';
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
